6eb2afffcaa82b6d16475f8e72a02bfd07f481fb
sujata <sujata.nepal@bajratechnologies.com>
2023-07-07 11:32:25 +0545

    added class

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const score = document.querySelector(".score");
 const healthLine = document.querySelector(".healthLine");
 const popUp = document.querySelector(".popUp");
 const gameArea = document.querySelector(".gameArea");
+
+console.log(score);
