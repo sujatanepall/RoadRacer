683f0adbf60af01f343094d6f81999bb4ff138ae
sujata <sujata.nepal@bajratechnologies.com>
2023-07-07 11:27:02 +0545

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const score = document.querySelector(".score");
+const healthLine = document.querySelector(".healthLine");
+const popUp = document.querySelector(".popUp");
+const gameArea = document.querySelector(".gameArea");
