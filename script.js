800f9311f43a7a3eb056e6530ec21bd633ddace1
sujata <sujata.nepal@bajratechnologies.com>
2023-07-07 14:07:38 +0545

    added gameArea

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const healthLine = document.querySelector(".healthLine");
 const popUp = document.querySelector(".popUp");
 const gameArea = document.querySelector(".gameArea");
 
-// console.log(gameArea);
+console.log(gameArea);
+
 popUp.addEventListener("click", start);
-let player = {};
+
+let player = { speed: 5 };
 
 let keys = {
   ArrowUp: false,
@@ -32,12 +34,40 @@ function keyUp(e) {
 
 function gamePlay() {
   console.log("Game clicked");
+  let car = document.querySelector(".car");
   if (player.start) {
+    if (keys.ArrowUp) {
+      player.y -= player.speed;
+    }
+    if (keys.ArrowDown) {
+      player.y += player.speed;
+    }
+    if (keys.ArrowLeft) {
+      player.x -= player.speed;
+    }
+    if (keys.ArrowRight) {
+      player.x += player.speed;
+    }
+    car.style.top = player.y + "px";
+    car.style.left = player.x + "px";
+
     window.requestAnimationFrame(gamePlay);
   }
 }
 function start() {
   gameArea.classList.remove("hide");
+  popUp.classList.add("hide");
   player.start = true;
   window.requestAnimationFrame(gamePlay);
+
+  let car = document.createElement("div");
+  car.setAttribute("class", "car");
+  //   car.innerText = "Car";
+  gameArea.appendChild(car);
+
+  player.x = car.offsetLeft;
+  player.y = car.offsetTop;
+
+  console.log("top position" + car.offsetTop);
+  console.log("left position" + car.offsetLeft);
 }
